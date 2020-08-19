263b7b43e79a4e4c93424c10bdac5498e3373f36
Ali Ghezel <ali.nov7@gmail.com>
2020-08-19 16:41:18 +0200

    Printing in the correct way HydroUnitBlock variables

--- ucblock_solver.cpp
+++ ucblock_solver.cpp
@@ -322,6 +322,40 @@ int main( int argc, char ** argv ) {
    auto battery_unit_block = dynamic_cast<BatteryUnitBlock *>(unit_block);
    if( battery_unit_block != nullptr ) {
 
+
+    auto active_power = battery_unit_block->get_active_power(0);
+    std::cout << "active_power  = [";
+    for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
+     std::cout << std::setw( 20 ) <<  active_power[t].get_value();
+    }
+    std::cout << " ]" << std::endl;
+
+
+    auto PrimarySR = battery_unit_block->get_primary_spinning_reserve( 0 );
+
+    std::cout << "PrimarySR     = [";
+
+    for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
+
+     std::cout << std::setw( 20 ) <<  PrimarySR[t].get_value();
+
+    }
+
+    std::cout << " ]" << std::endl;
+
+
+    auto SecondarySR = battery_unit_block->get_secondary_spinning_reserve( 0 );
+
+    std::cout << "SecondarySR     = [";
+
+    for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
+
+     std::cout << std::setw( 20 ) <<  SecondarySR[t].get_value();
+
+    }
+
+    std::cout << " ]" << std::endl;
+
     auto Intake_level = battery_unit_block->get_intake_level();
     std::cout << "IntakeLevel[:Storage]  = [";
     for( auto & t : Intake_level ) {
@@ -357,7 +391,7 @@ int main( int argc, char ** argv ) {
    if( hydro_unit_block != nullptr ) {
     for( UnitBlock::Index g = 0; g < unit_block->get_number_generators(); ++g ) {
      auto active_power = hydro_unit_block->get_active_power( g );
-     std::cout << "active_power     = [";
+     std::cout << "active_power [" + std::to_string( g ) + "]" " = [";
      for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
       std::cout << std::setw( 20 ) << active_power[t].get_value();
      }
@@ -367,7 +401,7 @@ int main( int argc, char ** argv ) {
 
     for( UnitBlock::Index l = 0; l < hydro_unit_block->get_number_generators(); ++l ) {
      auto flow_rate = hydro_unit_block->get_flow_rate( l );
-      std::cout << "FlowRate   = [";
+     std::cout << "FlowRate [" + std::to_string( l ) + "]" " = [";
      for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
       std::cout << std::setw( 25 ) << std::setprecision( 14 ) << flow_rate[t].get_value();
      }
@@ -376,7 +410,7 @@ int main( int argc, char ** argv ) {
 
     for( UnitBlock::Index n = 0; n < hydro_unit_block->get_number_reservoirs(); ++n ) {
      auto volumetric = hydro_unit_block->get_volumetric( n );
-      std::cout << "Volumetric  = [";
+     std::cout << "Volumetric [" + std::to_string( n ) + "]" " = [";
      for( UnitBlock::Index t = 0; t < unit_block->get_time_horizon(); ++t ) {
       std::cout << std::setw( 25 ) << std::setprecision( 14 ) << volumetric[t].get_value();
      }
