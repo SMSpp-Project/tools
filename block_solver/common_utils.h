03880ab4aa93f8032ff4ac4fba381577238ed138
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-06-17 19:06:35 +0200

    Improved text message

--- block_solver/common_utils.h
+++ block_solver/common_utils.h
@@ -245,13 +245,14 @@ void process_args( int argc , char ** argv )
 
 void smspp_terminate()
 {
- std::cerr << "Uncaught exception in executing SMS++: ";
+ std::cerr << "Uncaught exception in executing SMS++:\n";
  try {
   std::rethrow_exception(std::current_exception());
  } catch (const std::exception& e) {
-  std::cerr << e.what() << std::endl;
+  std::cerr << "\tException type: " << typeid(e).name() << "\n";
+  std::cerr << "\tException message: " << e.what() << "\n";
  } catch (...) {
-  std::cerr << "Unknown exception" << std::endl;
+  std::cerr << "\tUnknown exception" << std::endl;
  }
  std::abort(); // or exit(1)
 }
