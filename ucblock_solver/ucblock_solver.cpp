0805edcbc8be946233b50ae5b65311648956ceab
Donato Meoli <donato.meoli.95@gmail.com>
2026-09-10 21:22:36 +0200

    ucblock_solver and svm_solver set the Solver log like the other tools do, so that intLogVerb has something to print onto

--- ucblock_solver/ucblock_solver.cpp
+++ ucblock_solver/ucblock_solver.cpp
@@ -202,6 +202,9 @@ int main( int argc , char ** argv )
  // solve - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  std::cout.setf( std::ios::scientific, std::ios::floatfield );
  std::cout << std::setprecision( 8 );
+
+ set_solver_logs( block );
+
  solve_all( block );
 
  // cleanup - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
