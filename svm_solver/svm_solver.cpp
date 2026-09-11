159af673f6b17aabe069dc8243fba887a4031ef2
Donato Meoli <donato.meoli.95@gmail.com>
2026-09-11 20:45:40 +0200

    svm_solver: the definitions of the FastFlow graph helpers, which MSVC needs

--- svm_solver/svm_solver.cpp
+++ svm_solver/svm_solver.cpp
@@ -74,6 +74,10 @@
 
 #include <ff/parallel_for.hpp>
 
+// ff/pipeline.hpp declares the static isa2a_get*set() helpers, which are only
+// defined in ff/graph_utils.hpp: MSVC rejects the undefined static with C2129
+#include <ff/graph_utils.hpp>
+
 #include <SMOSolver.h>
 #include <SVCBlock.h>
 #include <SVRBlock.h>
